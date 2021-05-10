809f8118e5f833b43fa7effde0a1d5c45b8eaa6d
l337pino <1337pino@gmail.com>
2021-05-10 14:50:48 -0700

    19. Remove Nth Node From End of List java solution

---
+++ Problems/Medium/_19_RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,53 @@
+/** 19. Remove Nth Node From End of List
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ */
+
+package Problems.Medium;
+
+import java.util.*;
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class _19_RemoveNthNodeFromEndOfList {
+    /**
+     * Remove the nth node from the end of the linked list of ListNode objects and return its head.
+     * @param head Root of the linked list.
+     * @param n Location, from the back of the list, to remove.
+     * @return The root of the linked list.
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        Stack<ListNode> nodeStack = new Stack<ListNode>();
+        ListNode walker = head;
+        
+        // Store all of the nodes into a stack
+        while (walker != null) {
+            nodeStack.push(walker);
+            walker = walker.next;
+        }
+        
+        ListNode nPlus1 = null;
+        
+        // Pop nodes off of the stack until the top of the stack is the nth node
+        for (int i = 1; i < n; i++) {
+            nPlus1 = nodeStack.pop();
+        }
+        
+        // Get rid of the nth node
+        nodeStack.pop();
+        
+        // If the root node was the nth node
+        if (nodeStack.size() == 0) return nPlus1;
+        
+        ListNode nMinus1 = nodeStack.pop();
+        nMinus1.next = nPlus1;
+        
+        return head;
+    }
+}
